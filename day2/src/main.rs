6025d122db39a32b8aa6e6a1a07a8981864db0ed
David Visscher <david@davidvisscher.nl>
2020-12-04 16:07:01 +0100

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -11,15 +11,23 @@ fn main() {
     let mut correct_passwords = 0;
 
     for password in passwords.iter() {
-        if check_password(&password) {
+        if check_password_pt1(&password) {
             correct_passwords += 1;
         }
     }
+    println!("Correct Passwords (Part 1): {}", correct_passwords);
 
-    println!("Correct Passwords: {}", correct_passwords)
+    correct_passwords = 0;
+
+    for password in passwords.iter() {
+        if check_password_pt2(&password) {
+            correct_passwords += 1;
+        }
+    }
+    println!("Correct Passwords (Part 2): {}", correct_passwords);
 }
 
-fn check_password(password_line: &String) -> bool {
+fn check_password_pt1(password_line: &String) -> bool {
     // Split line on whitespace.
     // This way we end up with
     // 0: number-number
@@ -43,6 +51,36 @@ fn check_password(password_line: &String) -> bool {
     }
     
     if occurrences >= minimum && occurrences <= maximum {
+        return true;
+    }
+    return false;
+}
+
+fn check_password_pt2(password_line: &String) -> bool {
+    // Split line on whitespace.
+    // This way we end up with
+    // 0: number-number
+    // 1: letter:
+    // 2: password
+    let split: Vec<&str> = password_line.split(" ").collect();
+
+    let positions: Vec<&str> = split[0].trim().split("-").collect();
+    let p1: usize = positions[0].parse::<usize>().unwrap();
+    let p2: usize = positions[1].parse::<usize>().unwrap();
+
+    let limited_character = String::from(split[1].trim().replace(":", ""));
+    let password = String::from(split[2].trim());
+
+    let mut occurrences: u64 = 0;
+
+    for (char_index, character) in password.char_indices() {
+        if char_index+1 == p1 || char_index+1 == p2 {
+            if String::from(character) == limited_character {
+                occurrences += 1;
+            }
+        }
+    }
+    if occurrences == 1 {
         return true;
     }
     return false;
