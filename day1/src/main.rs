9b2c9663a62bc2e8eae3a277cb99b15c639ef880
David Visscher <david@davidvisscher.nl>
2020-12-04 15:16:28 +0100

    Add day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+
 fn main() {
-    println!("Hello, world!");
+    let numberfile = File::open("numbers.dat").expect("Unable to open file.");
+    let reader = BufReader::new(numberfile);
+
+    let numbers: Vec<u64> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<u64>().unwrap())
+        .collect();
+
+    for x in numbers.iter() {
+        for y in numbers.iter() {
+            if x + y == 2020 {
+                println!("{} {} = 2020, {} * {} = {}", x, y, x, y, x*y);
+            }
+        }
+    }
 }
