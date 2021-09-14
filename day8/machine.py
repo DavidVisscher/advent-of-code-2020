691ec9994a8a7452cd79d06fc31e9a14c6453e54
David Visscher <david@davidvisscher.nl>
2021-09-14 08:54:31 +0200

    Move instruction to its own file

--- day8/machine.py
+++ day8/machine.py
@@ -1,14 +1,4 @@
-class Instruction:
-    def __init__(self, name, argument):
-        self.name = name
-        self.argument = argument
-
-    def __eq__(self, other) -> bool:
-        if isinstance(other, Instruction) and \
-                self.name == other.name and \
-                self.argument == other.argument:
-            return True
-        return False
+from .instruction import Instruction
 
 
 class JumpOutOfBoundsException(Exception):
